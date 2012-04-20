4aecbba9dc9ea8a44566c625fb061444c006b0d1
Adrian Maurer <adrian.maurer@gmail.com>
2012-04-20 16:23:28 -0400

    get active pages and compare

--- application/modules/admin/controllers/DashboardController.php
+++ application/modules/admin/controllers/DashboardController.php
@@ -11,8 +11,19 @@ class Admin_DashboardController extends Fancrank_Admin_Controller_BaseController
     public function fanpagesAction() 
     {
         $fanpages_model = new Model_Fanpages;
-        $facebook_request = $fanpages_model->facebookRequest('me', $this->_identity->user_access_token, array('accounts'));
-        $this->view->accounts = $facebook_request->accounts->data;
+        $fanpages = $fanpages_model->facebookRequest('me', $this->_identity->user_access_token, array('accounts'));
+        
+        $authorized_pages = $fanpages_model->getAuthorizedFanpages($this->_identity->user_id);
+
+        if (count($authorized_pages)) {
+            foreach($authorized_pages as $authorized_page) {
+                $authorized_ids[] = $authorized_page->fanpage_id;
+            }
+        } else {
+             $authorized_ids = array();
+        }
+        $this->view->accounts = $fanpages->accounts->data;
+        $this->view->authorized = $authorized_ids;
     }
 
     public function myaccountAction()
