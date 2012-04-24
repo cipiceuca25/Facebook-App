0b54c8bdf1cd04d7ad328d0e4cfe67e4a6c350bd
Adrian Maurer <adrian.maurer@gmail.com>
2012-04-24 12:03:02 -0400

    landing page for app

--- application/modules/app/controllers/IndexController.php
+++ application/modules/app/controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-class App_IndexController extends Fancrank_Admin_Controller_BaseController
+class App_IndexController extends Fancrank_App_Controller_BaseController
 {
 
     public function indexAction()
--- library/Fancrank/App/Controller/BaseController.php
+++ library/Fancrank/App/Controller/BaseController.php
@@ -2,13 +2,14 @@
 
 abstract class Fancrank_App_Controller_BaseController extends Fancrank_Controller_Action
 {
-	public function preDispatch() {
-		die("ere");
+	public function preDispatch() 
+	{
+		parent::preDispatch();
 	}
+
     public function init() 
     {
-        //add the resource specific javascript file to the layout
-        $this->view->headScript()->appendFile('/js/app/'. $this->_request->getControllerName() . '.js');
+        parent::init();
     }
 }
 
