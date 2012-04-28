d5ccf968cd74790522e4482bf74275a0068b8299
Adrian Maurer <adrian.maurer@gmail.com>
2012-04-28 19:37:21 -0400

    show response in view

--- application/modules/app/controllers/AppController.php
+++ application/modules/app/controllers/AppController.php
@@ -9,13 +9,12 @@ class App_AppController extends Fancrank_App_Controller_BaseController
 
   	public function topfansAction()
   	{
-  		die('jere');
+
   		$model = new Model_TopFans;
   		$this->view->top_fans = $model->getTopFans($this->_getParam('id'));
   		$this->view->most_popular = $model->getMostPopular($this->_getParam('id'));
-  		//$this->view->top_talker = $model->getTopTalker($this->_getParam('id'));
-
-  		die(print_r($this->view->most_popular));
+  		$this->view->top_talker = $model->getTopTalker($this->_getParam('id'));
+      $this->view->top_clicker = $model->getTopClicker($this->_getParam('id'));
   	}
 }
 
